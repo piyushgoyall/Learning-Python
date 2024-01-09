40ae1ba7714b0c95a59a0f638758aaa3cf1f5fb7
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-01-09 19:26:48 +0530

    new

---
+++ practice/09-01-2024.py
@@ -0,0 +1,65 @@
+list1 = [1,2,3,4,5,6,7,8,9,10]
+for x in list1:
+    print(x, end = " ")
+
+# loc = 'Bank'
+# if loc == 'Auto Shop':
+#     print("Welcome to the auto shop")
+# elif loc == 'Bank':
+#     print("Welcome to the bank")
+# else:
+#     print("Where are you?")
+
+# x = False
+# y = 1
+# if x:
+#     print("x is True!")
+# else:
+#     print(y,"I will be printed in any case where x is not true")
+
+
+# x = False
+# if 1+2+3:
+#     print("x is True!")
+# else:
+#     print("I will be printed in any case where x is not true")
+
+
+# x = False
+# if x:
+#     print("x is True!")
+# else:
+#     print("I will be printed in any case where x is not true")
+
+# x = 1 + 4/3 -7
+# print(x)
+# print(type(x))
+
+# x = 1+'a'
+# print(x)
+
+# x = 1
+# print(x)
+
+# name = input("Enter your name: ")
+# print("Hello " + name)
+
+# first_name = "Tony"
+# last_name = "Stark"
+# age = 51
+# is_genius = True
+
+
+
+# is_adult = False
+
+# name = "Shradha"
+# age = 22
+
+# # name = "Aman"
+# # age = 24
+
+# print(name)
+# print(age)
+
+# print("Hello World")
