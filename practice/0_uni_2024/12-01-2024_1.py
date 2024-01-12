67e321eddcc1f55ca5e82b0c65cb59ca4e65155c
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-01-12 23:22:44 +0530

    new

---
+++ practice/0_uni_2024/12-01-2024_1.py
@@ -0,0 +1,38 @@
+# s = "Hello World"
+
+# print(s)
+# print(s[-1])
+# print(s[1:])
+# print(s[1:2])
+# print(s[:])
+# print(s[::1])
+# print(s[::2])
+# print(s[::-1])
+
+
+
+# concatenate string
+# s = s + " concatenate me!"
+# print(s)
+
+# letter = 'z'
+# print(letter*10)
+
+# print(s.split('W'))
+
+# print(s.find('World'))
+
+
+# s = "anoop.dobhala@chitkara.edu.co.in 12-01-2024 18:01"
+# ind1 = s.find('@')
+# ind2 = s.find(" ")
+# print(s[ind1+1 : ind2])
+
+# s = s.split('@')
+# print(s[1].split()[0])
+
+
+d = {}
+d['animal'] = 'Dog'
+d['answer'] = [1,2,3,4,5,['a','b','c','d']]
+print(d)
