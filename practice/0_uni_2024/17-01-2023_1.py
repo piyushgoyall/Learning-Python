5ad6d0e746c2bfcb6e12a8469494839b29be2498
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-01-17 19:09:36 +0530

    new

---
+++ practice/0_uni_2024/17-01-2023_1.py
@@ -0,0 +1,21 @@
+# Set is used to remove duplicate values from the list.
+
+list1 = ["rose",10,20,10,"swag","chitkara"]
+print(set(list1))
+print(list1)
+
+# set = {"Rose",10,20,"Swag","Chitkara"}
+# print(set)
+
+# myTuple = ("Rose","Swag",85.6,10,"Chitkara")
+# print(myTuple[4][5])
+# tuple[3] = 1000
+# print(myTuple[0])
+# print(myTuple[1][1:4])
+# print(myTuple[1][1])
+# print(myTuple[2][1:4])
+# print(myTuple[3])
+# print(myTuple[4])
+# print(myTuple[0:5])
+# print(myTuple[1:4])
+# print(myTuple[2:3])
